5b4fddcd734c37c53dbe682ff17e7cb26ffe916c
katy cmh <katy@cmh.tv>
2024-03-02 15:31:15 +0500

    new add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 == number2 * number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
